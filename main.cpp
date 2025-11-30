e0bfa1d1f624719cd81cea077b56c15e0601250f
IgorPolajzer <polajzerigor@gmail.com>
2025-11-30 15:59:36 +0100

    Working implementation with the pdf example

--- main.cpp
+++ main.cpp
@@ -75,8 +75,7 @@ void compress(const string& inputFile, const string& outputFile) {
         char c;
         int E3_counter = 0;
 
-        while (input.get(c))
-        {
+        while (input.get(c)) {
             int step = (upperBound - lowerBound + 1) / cummulativeFrequency;
             upperBound = lowerBound + step * table[c].upperBound - 1;
             lowerBound = lowerBound + step * table[c].lowerBound;
@@ -90,9 +89,7 @@ void compress(const string& inputFile, const string& outputFile) {
                         output << 1;
                     }
                     E3_counter = 0;
-                }
-
-                if (lowerBound >= secondQuarter) {
+                } else if (lowerBound >= secondQuarter) {
                     lowerBound = 2 * (lowerBound - secondQuarter);
                     upperBound = 2 * (upperBound - secondQuarter) + 1;
                     output << 1;
@@ -139,19 +136,23 @@ ordered_map<char, TableEntry> parseHeader(ifstream& input, int &bitSize) {
     bitSize = stoi(stringBitSize);
 
     // Parse used characters (all non-digits)
-    while (isalpha(input.peek())) {
+    while (true) {
+        if (!isalpha(input.peek()) && input.peek() != ',') break; // stop at first non-letter/non-comma
         input.get(c);
+        if (c == ',') continue;
         table[c] = TableEntry{c, -1, -1, -1};
     }
 
     // Parse character frequencies.
     int previousUpper = -1;
     for (auto & it : table) {
-        input.get(c);
-        string freq(1, c);
+        string freqStr;
+        while (input.get(c) && c != ',') {
+            freqStr += c;
+        }
 
-        int currentFreq = stoi(freq);
-        int lowerBound = previousUpper == - 1 ? 0 : previousUpper;
+        int currentFreq = stoi(freqStr);
+        int lowerBound = previousUpper == -1 ? 0 : previousUpper;
         int upperBound = lowerBound + currentFreq;
 
         it.second.frequency = currentFreq;
@@ -164,17 +165,72 @@ ordered_map<char, TableEntry> parseHeader(ifstream& input, int &bitSize) {
     return table;
 }
 
+char getCharFromTable(ordered_map<char, TableEntry>& table, int v) {
+    for (auto & it : table) {
+        if (it.second.lowerBound <= v  && it.second.upperBound > v) {
+            return it.first;
+        }
+    }
+}
+
+int readNextBit(ifstream& input) {
+    char bit_char;
+    if (input.get(bit_char)) {
+        if (bit_char == '0') return 0;
+        if (bit_char == '1') return 1;
+    }
+    return 0;
+}
+
 void decompress(const string& inputFile, const string& outputFile) {
     ifstream input(inputFile, ios::binary);
     ofstream output(outputFile, ios::binary);
 
     if (input.is_open() && output.is_open()) {
         // Parse the file header.
-        int field;
-        ordered_map<char, TableEntry> table = parseHeader(input, field);
+        int bitSize;
+        ordered_map<char, TableEntry> table = parseHeader(input, bitSize);
+        int cummulativeFrequency = getComulativeFrequency(table);
 
-        char c;
+        int field = 0;
+        for (int i = 0; i < bitSize - 1; i++ ) field = ( field << 1 ) | readNextBit(input);
 
+        int lowerBound = 0;
+        int upperBound = pow(2, bitSize - 1) - 1;
+        int secondQuarter = (upperBound + 1) / 2;
+        int firstQuarter = secondQuarter / 2;
+        int thirdQuarter = firstQuarter * 3;
+
+        for (int i = 0; i < cummulativeFrequency; i++) {
+            int step = (upperBound - lowerBound + 1) / cummulativeFrequency;
+            int v = (field - lowerBound) / step;
+            char currentChar = getCharFromTable(table, v);
+            output << currentChar;
+
+            upperBound = lowerBound + step * table[currentChar].upperBound - 1;
+            lowerBound = lowerBound + step * table[currentChar].lowerBound;
+
+            while (upperBound < secondQuarter || lowerBound >= secondQuarter) {
+                if (upperBound < secondQuarter) {
+                    lowerBound *= 2;
+                    upperBound = upperBound * 2 + 1;
+
+                    field = 2 * field + readNextBit(input);
+                } else if (lowerBound >= secondQuarter) {
+                    lowerBound = 2 * (lowerBound - secondQuarter);
+                    upperBound = 2 * (upperBound - secondQuarter) + 1;
+
+                    field = 2 * (field - secondQuarter) + readNextBit(input);
+                }
+            }
+
+            while (lowerBound >= firstQuarter && upperBound < thirdQuarter) {
+                lowerBound = 2 * (lowerBound - firstQuarter);
+                upperBound = 2 * (upperBound - firstQuarter) + 1;
+
+                field = 2 * (field - firstQuarter) + readNextBit(input);
+            }
+        }
     } else {
         cerr << "Error opening one of the files: " << inputFile << ", " << outputFile << endl;
     }
